d91fbe3a6c034441d3228b5266e0bc544da6ff30
ItsDevPlayz <77641792+ItsDevPlayz@users.noreply.github.com>
2021-11-26 11:43:10 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -17,4 +17,26 @@ document.getElementById("result").innerHTML='<img id="captured_img" src="'+data_
 
     });
 }
-classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Hpb_BkW2-/mode.json",modelLoaded);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Hpb_BkW2-/model.json",modelLoaded);
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    
+    }
+    
+    function check()
+    {
+    img=document.getElementById('captured_img');
+    classifier.classify(img, gotResult);
+    }
+    
+    function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML=results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(2);
+    }
+    }
